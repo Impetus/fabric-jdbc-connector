20343632fbdd9dcc1642ae0600bcd8d9ac73f890
unknown <hosur.narhari@Impetus-B0103.impetus.co.in>
2017-10-20 12:26:39 +0530

    poc implementation of sqlobjects

---
+++ src/main/java/com/impetus/fabric/jdbc/schema/FabricMetadata.java
@@ -0,0 +1,33 @@
+package com.impetus.fabric.jdbc.schema;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.impetus.blkch.sql.schema.ColumnType;
+import com.impetus.blkch.sql.schema.Metadata;
+import com.impetus.blkch.sql.schema.MetadataManager;
+import com.impetus.blkch.sql.schema.Table;
+
+public class FabricMetadata implements Metadata {
+	
+	private List<Table> tables = new ArrayList<Table>();
+
+	public FabricMetadata() {
+		Table table1 = new FabricTable("table1", Arrays.asList(new FabricColumn("col1", ColumnType.INT), new FabricColumn("col2", ColumnType.STRING), new FabricColumn("col3", ColumnType.DOUBLE)));
+		Table table2 = new FabricTable("table2", Arrays.asList(new FabricColumn("col4", ColumnType.INT), new FabricColumn("col5", ColumnType.STRING)));
+		tables.add(table1);
+		tables.add(table2);
+		
+	}
+
+	public Table getTable(String tableName) {
+		for(Table table : tables) {
+			if(table.getName().equalsIgnoreCase(tableName)) {
+				return table;
+			}
+		}
+		throw new RuntimeException("Table " + tableName + " doesn't exist");
+	}
+
+}
---
+++ src/main/java/com/impetus/fabric/jdbc/schema/FabricTable.java
@@ -0,0 +1,44 @@
+package com.impetus.fabric.jdbc.schema;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.impetus.blkch.sql.schema.Column;
+import com.impetus.blkch.sql.schema.Table;
+
+public class FabricTable implements Table {
+	
+	private String name;
+	
+	private List<FabricColumn> columns;
+	
+	public FabricTable(String name, List<FabricColumn> columns) {
+		this.name = name;
+		this.columns = columns;
+		for(FabricColumn column : columns) {
+			column.setTable(this);
+		}
+	}
+
+	public Column getColumnByName(String columnName) {
+		for(Column column : columns) {
+			if(column.getName().equalsIgnoreCase(columnName)) {
+				return column;
+			}
+		}
+		throw new RuntimeException("Column " + columnName + " doesn't exist in table " + name);
+	}
+
+	public List<Column> getColumns() {
+		List<Column> cols = new ArrayList<Column>();
+		for(Column column : columns) {
+			cols.add(column);
+		}
+		return cols;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
