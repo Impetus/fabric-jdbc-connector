33c95f2c4ea5fd871b3fc2f719cfeba1d531e276
Hosur Narahari <hosur.narhari@impetus.co.in>
2017-11-21 11:29:24 +0530

    committing support for star item in select clause

--- src/main/java/com/impetus/fabric/parser/DataFrame.java
+++ src/main/java/com/impetus/fabric/parser/DataFrame.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import com.impetus.blkch.sql.query.Column;
+import com.impetus.blkch.sql.query.IdentifierNode;
+import com.impetus.blkch.sql.query.SelectItem;
+import com.impetus.blkch.sql.query.StarNode;
+
 public class DataFrame {
 
 	private List<String> columns;
@@ -35,24 +40,36 @@ public class DataFrame {
 		return data;
 	}
 
-	public DataFrame select(List<String> cols) {
+	public DataFrame select(List<SelectItem> cols) {
 		List<List<Object>> returnData = new ArrayList<>();
+		List<String> returnCols = new ArrayList<>();
 		for(List<Object> record : data) {
 			List<Object> returnRec = new ArrayList<>();
-			for(String col : cols) {
-				int colIndex;
-				if(columns.contains(col)) {
-					colIndex = columns.indexOf(col);
-				} else if(aliasMapping.containsKey(col)) {
-					String actualCol = aliasMapping.get(col);
-					colIndex = columns.indexOf(actualCol);
-				} else {
-					throw new RuntimeException("Column " + col + " doesn't exist in table");
+			for(SelectItem col : cols) {
+				if(col.hasChildType(StarNode.class)) {
+					for(String colName : columns) {
+						int colIndex = columns.indexOf(colName);
+						returnRec.add(record.get(colIndex));
+						returnCols.add(colName);
+					}
+				} else if(col.hasChildType(Column.class)) {
+					int colIndex;
+					String colName = col.getChildType(Column.class, 0).getChildType(IdentifierNode.class, 0).getValue();
+					if(columns.contains(colName)) {
+						colIndex = columns.indexOf(colName);
+						returnCols.add(colName);
+					} else if(aliasMapping.containsKey(colName)) {
+						String actualCol = aliasMapping.get(colName);
+						colIndex = columns.indexOf(actualCol);
+						returnCols.add(actualCol);
+					} else {
+						throw new RuntimeException("Column " + colName + " doesn't exist in table");
+					}
+					returnRec.add(record.get(colIndex));
 				}
-				returnRec.add(record.get(colIndex));
 			}
 			returnData.add(returnRec);
 		}
-		return new DataFrame(returnData, cols, aliasMapping);
+		return new DataFrame(returnData, returnCols, aliasMapping);
 	}
 }
