f1b887dafa998daba56f7fea6a71eed0557101ae
jeetendra.jaiswal <jeetendra.jaiswal@impetus.co.in>
2018-03-05 19:45:44 +0530

    Unit Test with Group by and Having With Count Function

--- src/test/java/com/impetus/fabric/query/QueryBlockIT.java
+++ src/test/java/com/impetus/fabric/query/QueryBlockIT.java
@@ -21,18 +21,31 @@ public class QueryBlockIT extends TestCase {
         Class.forName("com.impetus.fabric.jdbc.FabricDriver");
         File configFolder = new File("src/test/resources/blockchain-query");
         String configPath = configFolder.getAbsolutePath();
-        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Swati Raj", "");
+        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Impetus User", "");
         Statement stat = conn.createStatement();
-        ResultSet rs = stat.executeQuery("select * from block where blockNo=2"); // This is dummy query
+        ResultSet rs = stat.executeQuery("select * from block where blockNo = 2"); // This is dummy query
         assert(rs.next());
     }
+
+
+    @Test
+    public void testFabricStatementWithGroupByAndHaving() throws ClassNotFoundException, SQLException {
+        Class.forName("com.impetus.fabric.jdbc.FabricDriver");
+        File configFolder = new File("src/test/resources/blockchain-query");
+        String configPath = configFolder.getAbsolutePath();
+        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Impetus User", "");
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery("select blockNo,Count(blockNo) from block where blockNo = 2 group by blockNo having Count(blockNo) = 1 "); // This is dummy query
+        assert(rs.next());
+    }
+
 
     @Test
     public void testFabricStatementWithOrderByAndGroupBy() throws ClassNotFoundException, SQLException {
         Class.forName("com.impetus.fabric.jdbc.FabricDriver");
         File configFolder = new File("src/test/resources/blockchain-query");
         String configPath = configFolder.getAbsolutePath();
-        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Swati Raj", "");
+        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Impetus User", "");
         Statement stat = conn.createStatement();
         ResultSet rs = stat.executeQuery("select * from block where blockNo >= 2 and blockNo <= 5 groupby('blockno') order by blockNo desc");
         assert(rs.next());
@@ -45,7 +58,7 @@ public class QueryBlockIT extends TestCase {
         Class.forName("com.impetus.fabric.jdbc.FabricDriver");
         File configFolder = new File("src/test/resources/blockchain-query");
         String configPath = configFolder.getAbsolutePath();
-        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Swati Raj", "");
+        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Impetus User", "");
         Statement stat = conn.createStatement();
         String createFuncQuery = "CREATE FUNCTION chncodefunc"+currentTimeStamp+" AS 'hyperledger/fabric/examples/chaincode/go/chaincode_example02' WITH VERSION '1.0'"
                 + " WITH ARGS a, 500, b, 200";
@@ -59,7 +72,7 @@ public class QueryBlockIT extends TestCase {
         Class.forName("com.impetus.fabric.jdbc.FabricDriver");
         File configFolder = new File("src/test/resources/blockchain-query");
         String configPath = configFolder.getAbsolutePath();
-        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Swati Raj", "");
+        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Impetus User", "");
         Statement stat = conn.createStatement();
         String createFuncQuery = "CREATE FUNCTION chncodefunc"+currentTimeStamp+" AS 'hyperledger/fabric/examples/chaincode/go/chaincode_example02' WITH VERSION '1.0'"
                 + " WITH ARGS a, 500, b, 200";
@@ -77,7 +90,7 @@ public class QueryBlockIT extends TestCase {
         Class.forName("com.impetus.fabric.jdbc.FabricDriver");
         File configFolder = new File("src/test/resources/blockchain-query");
         String configPath = configFolder.getAbsolutePath();
-        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Swati Raj", "");
+        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Impetus User", "");
         Statement stat = conn.createStatement();
 
         String createFuncQuery = "CREATE FUNCTION chncodefunc"+currentTimeStamp+" AS 'hyperledger/fabric/examples/chaincode/go/chaincode_example02' WITH VERSION '1.0'"
@@ -94,7 +107,7 @@ public class QueryBlockIT extends TestCase {
         Class.forName("com.impetus.fabric.jdbc.FabricDriver");
         File configFolder = new File("src/test/resources/blockchain-query");
         String configPath = configFolder.getAbsolutePath();
-        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Swati Raj", "");
+        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Impetus User", "");
         Statement stat = conn.createStatement();
         ResultSet rs = stat.executeQuery("select * from block blk where blockNo = 200 And blockNo>=2 blockNo <=300"); // This is dummy query
         assert(rs.next());
@@ -107,7 +120,7 @@ public class QueryBlockIT extends TestCase {
         Class.forName("com.impetus.fabric.jdbc.FabricDriver");
         File configFolder = new File("src/test/resources/blockchain-query");
         String configPath = configFolder.getAbsolutePath();
-        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Swati Raj", "");
+        Connection conn = DriverManager.getConnection("jdbc:fabric://" + configPath+":mychannel", "Impetus User", "");
         Statement stat = conn.createStatement();
 
         ResultSet rs = stat.executeQuery("select * from block blk where blockNo >= -2 and blockNo <= 1"); // This is dummy query
