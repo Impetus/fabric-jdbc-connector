dd117ff132d3bddd1fb01729ddf8ce17dbeb7f77
Hosur Narahari <hosur.narhari@impetus.co.in>
2018-06-05 14:42:30 +0530

    committing etltracker chaincode

---
+++ fabric-sample/src/main/resources/etltracker/etltracker.go
@@ -0,0 +1,103 @@
+// etltracker.go
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+type Queries struct {
+	Database  string
+	Table     string
+	Query     string
+	timestamp time.Time
+}
+
+type EtlTracker struct {
+}
+
+func ToString(currentTime time.Time) string {
+	return fmt.Sprintf("%d-%d-%d %d:%d:%d", currentTime.Year(), currentTime.Month(), currentTime.Day(), currentTime.Hour(), currentTime.Minute(),
+		currentTime.Second())
+}
+
+func main() {
+	err := shim.Start(new(EtlTracker))
+	if err != nil {
+		fmt.Println("Error starting Etl Tracker chaincode %s", err)
+	}
+}
+
+func (et *EtlTracker) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("Init() Initialization Complete ")
+	return shim.Success(nil)
+}
+
+func (et *EtlTracker) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	function, args := stub.GetFunctionAndParameters()
+	if function == "insert" {
+		return et.Insert(stub, args)
+	}
+	if function == "query" {
+		return et.Query(stub, args)
+	}
+	if function == "queryHistory" {
+		return et.QueryHistory(stub, args)
+	}
+	return shim.Error("Invoke: Invalid function name " + function)
+}
+
+func (et *EtlTracker) Insert(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	database := args[0]
+	table := args[1]
+	query := args[2]
+	queries := Queries{database, table, query, time.Now()}
+	value := QueriesToCSV(queries)
+	compositeKey, _ := stub.CreateCompositeKey(database, []string{table})
+	err := stub.PutState(compositeKey, value)
+	if err != nil {
+		return shim.Error("Insert(): Error inserting change: " + err.Error())
+	}
+	return shim.Success(value)
+}
+
+func (et *EtlTracker) Query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	database := args[0]
+	table := args[1]
+	compositeKey, _ := stub.CreateCompositeKey(database, []string{table})
+	bytes, err := stub.GetState(compositeKey)
+	if err != nil {
+		return shim.Error("Query(): Error query current schema: " + err.Error())
+	}
+	return shim.Success(bytes)
+}
+
+func (et *EtlTracker) QueryHistory(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	database := args[0]
+	table := args[1]
+	compositeKey, _ := stub.CreateCompositeKey(database, []string{table})
+	history, err := stub.GetHistoryForKey(compositeKey)
+	if err != nil {
+		return shim.Error("QueryHistory(): Error quering history for the queried table: " + err.Error())
+	}
+	str := ""
+	for history.HasNext() {
+		keyChange, err := history.Next()
+		if err != nil {
+			return shim.Error("QueryHistory(): Error quering history for the queried table: " + err.Error())
+		}
+		value := keyChange.GetValue()
+		str = str + string(value) + "\n"
+	}
+	str = str[:len(str)-1]
+	history.Close()
+	return shim.Success([]byte(str))
+}
+
+func QueriesToCSV(queries Queries) []byte {
+	csv := queries.Database + ";" + queries.Table + ";" + queries.Query + ";" + ToString(queries.timestamp)
+	return []byte(csv)
+}
