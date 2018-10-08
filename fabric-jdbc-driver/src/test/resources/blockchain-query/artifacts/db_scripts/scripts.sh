349019b05f92e0ea82db8407c0ef4d66e7e4719f
Hosur Narahari <hosur.narhari@impetus.co.in>
2018-10-08 15:43:07 +0530

    committing integration test changes

--- fabric-jdbc-driver/src/test/resources/blockchain-query/artifacts/db_scripts/scripts.sh
+++ fabric-jdbc-driver/src/test/resources/blockchain-query/artifacts/db_scripts/scripts.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-sleep 10
-while !(mysqladmin -uroot -proot ping)
-do
-   sleep 5
-   echo "waiting for mysql ..."
-done
-#mysql -uroot  < /db_scripts/test_schema.sql
+/db_scripts/wait-for-it.sh localhost:5432 -t 60
